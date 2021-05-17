81e609b7feb680673e0cb99a562b63ff76c76ddd
Shaharyaar7 <shaharyaarkutchi7@gmail.com>
2021-05-17 21:47:31 +0530

    created modules

---
+++ govt.php
@@ -0,0 +1,127 @@
+<?php include('security.php'); ?>
+<?php include('includes/header.php');?>
+<?php include('includes/navbar.php');?>
+
+
+<br><br><br><br>
+
+  
+<style type="text/css">
+  
+.button{
+  background: #3498db;
+    color: #fff;
+    border-radius: 50px;
+    margin: 0 15px;
+    padding: 10px 30px; 
+}
+
+#services .box {
+    padding: 50px 20px;
+    margin-bottom: 50px;
+    text-align: center;
+    border: 1px solid #e6e6e6;
+    height: 170px;
+    position: relative;
+    background: #fafafa;
+}
+
+.icofont-navigation-menu:before {
+    content: "\efa2";
+    color: black;
+}
+
+#header.header-scrolled, #header.header-inner-pages {
+    background: white;
+}
+
+#header .logo a {
+    color: #000;
+}
+
+.nav-menu a {
+    display: block;
+    position: relative;
+    color: #000;
+    transition: 0.3s;
+    font-size: 15px;
+    font-family: "Open Sans", sans-serif;
+    font-weight: 600;
+}
+  
+</style>
+    <!-- bradcam_area_start  -->
+    <div class="bradcam_area breadcam_bg ">
+        <div class="container">
+          <br><br>
+            <div class="row">
+                <div class="col-lg-4">
+                    <div class="bradcam_text">
+                        <img src="img/chattisgarh/logo.png" class="img-fluid" >
+                        <h1>Government Services</h1>
+                    </div>
+                </div>
+                <div class="col-lg-8">
+                    <div class="bradcam_text">
+                      <br>
+                        <img src="img/chattisgarh/govt.jpg" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services">
+      <div class="container-fluid">
+
+         <div class="section-title">
+          <h2>Digital Chhattisgarh</h2>
+          <p>Our Services</p>
+        </div>
+
+        <div class="row justify-content-center">
+          <div class="col-xl-10">
+            <div class="row">
+              
+              <div class="col-lg-3 col-md-6 icon-box">
+                  <h4 class="title"><a href="https://www.utiitsl.com/UTIITSL_SITE/site/pan/index.html">PAN Card - Online Application</a></h4>
+              </div>
+               <div class="col-lg-3 col-md-6 icon-box">
+                  <h4 class="title"><a href="https://www.cgstate.gov.in/documents/10179/47c2dae7-452d-4ae0-9e1c-555d097629f2">Provisionally selected Chartered Accountant & Firms</a></h4>
+              </div>
+               <div class="col-lg-3 col-md-6 icon-box">
+                  <h4 class="title"><a href="https://www.cgstate.gov.in/documents/10179/191f6223-b4fc-4928-a870-d0b772ba8676">List of events to be organized by India Trade Promotion Organization in the year 2015</a></h4>
+              </div>
+               <div class="col-lg-3 col-md-6 icon-box">
+                  <h4 class="title"><a href="https://www.cgstate.gov.in/web/guest/law-order">Law & Order</a></h4>
+              </div>
+               <div class="col-lg-3 col-md-6 icon-box">
+                  <h4 class="title"><a href="https://www.cgstate.gov.in/documents/10179/7556f7f1-4bd2-4bc5-a6ef-ee0783c41c2f">Socio Economic and Caste Census 2011 - Quiz Schedule</a></h4>
+              </div>
+               <div class="col-lg-3 col-md-6 icon-box">
+                  <h4 class="title"><a href="https://www.cgstate.gov.in/documents/10179/06ccaf89-eefd-408a-83f7-30fce0165270">Socio Economic and Caste Census 2011  - Application Form for Claim Objection</a></h4>
+              </div>
+               <div class="col-lg-3 col-md-6 icon-box">
+                  <h4 class="title"><a href="https://www.cgstate.gov.in/documents/10179/c9aca5a1-cdcf-4c80-ab73-c708f1edd008">Scheme for supporting MSMEs in the electronics sector</a></h4>
+              </div>
+               <div class="col-lg-3 col-md-6 icon-box">
+                  <h4 class="title"><a href="https://lakshadweep.gov.in/service/exam-results/">Government office</a></h4>
+              </div>
+
+            
+          </div>
+        </div>
+      </div>
+
+      </div>
+    </section><!-- End Features Section -->
+
+
+<?php include('includes/scripts.php');?>
+<?php include('includes/footer.php');?>
+
+
+
+
+
---
+++ index.php
@@ -0,0 +1,399 @@
+<?php include('security.php'); ?>
+<?php include('includes/header.php');?>
+<?php include('includes/navbar.php');?>
+
+<style type="text/css">
+  #hero {
+  width: 100%;
+  height: 100vh;
+  background-position:center;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+</style>
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" style="background-image: url(img/chattisgarh/bg.jpg);">
+
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container-fluid">
+
+        <div class="row justify-content-center">
+          <div class="col-xl-5 col-lg-6 video-box d-flex justify-content-center align-items-stretch">
+            
+          </div>
+
+          <div class="col-xl-5 col-lg-6 icon-boxes d-flex flex-column align-items-stretch justify-content-center py-5 px-lg-5">
+            <h3>About Chhattisgarh</h3>
+            <p>Chhattisgarh is a heavily forested state in central India known for its temples and waterfalls. Near the capital Raipur, the town of Sirpur on the Mahanadi River is home to the red-brick Lakshmana (Laxman) Temple, decorated with carvings from Indian mythology. In the south, the city of Jagdalpur hosts the Sanjay Market on Sundays, a bartering place for local tribes. The huge Chitrakoot Falls lie to the northwest.</p>
+
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-right-arrow"></i></div>
+              <h4 class="title"><a href=""><strong> Area:</strong>  135,191 km²</a></h4>
+            </div>
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-right-arrow"></i></div>
+              <h4 class="title"><a href=""><strong>Population:</strong> 3.22 crores (2020)</a></h4>
+            </div>
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-right-arrow"></i></div>
+              <h4 class="title"><a href=""><strong>Chief minister:</strong> Bhupesh Baghel</a></h4>
+            </div>
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-right-arrow"></i></div>
+              <h4 class="title"><a href=""><strong>Governor:</strong>  Anusuiya Uikey</a></h4>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Counts Section ======= -->
+    <section id="counts" class="counts section-bg">
+      <div class="container-fluid">
+
+        <div class="row counters">
+
+          <div class="col-lg-3 col-6 text-center">
+            <span data-toggle="counter-up" id="StateConfirmed"></span>
+            <p>Covid19 Confirmed Cases</p>
+          </div>
+
+          <div class="col-lg-3 col-6 text-center">
+            <span data-toggle="counter-up" id="StateRecovered"></span>
+            <p>Covid19 Recovered Cases</p>
+          </div>
+
+          <div class="col-lg-3 col-6 text-center">
+            <span data-toggle="counter-up" id="StateActive"></span>
+            <p>Covid19 Active Cases</p>
+          </div>
+
+          <div class="col-lg-3 col-6 text-center">
+            <span data-toggle="counter-up" id="StateDeath"></span>
+            <p>Covid19 Death Cases</p>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Counts Section -->
+
+
+    <!-- ======= Cta Section ======= -->
+    <section id="cta" class="cta">
+      <div class="container" data-aos="zoom-in">
+
+        <div class="text-center">
+          <h3>Coronavirus</h3>
+          <p>Coronavirus disease (COVID-19) is an infectious disease caused by a newly discovered coronavirus.
+Most people infected with the COVID-19 virus will experience mild to moderate respiratory illness and recover without requiring special treatment.Older people, and those with underlying medical problems like cardiovascular disease, diabetes, chronic respiratory disease, and cancer are more likely to develop serious illness.<br><br>
+The COVID-19 virus spreads primarily through droplets of saliva or discharge from the nose when an infected person coughs or sneezes, so it’s important that you also practice respiratory etiquette (for example, by coughing into a flexed elbow).
+At this time, there are no specific vaccines or treatments for COVID-19. However, there are many ongoing clinical trials evaluating potential treatments. WHO will continue to provide updated information as soon as clinical findings become available.</p>
+        </div>
+
+      </div>
+    </section><!-- End Cta Section -->
+
+    <br><br><br>
+
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services">
+      <div class="container-fluid">
+
+         <div class="section-title">
+          <h2>Coronavirus Symptoms</h2>
+          <p>Check the symptoms</p>
+        </div>
+
+        <div class="row justify-content-center">
+          <div class="col-xl-10">
+            <div class="row">
+              
+              <div class="col-lg-4 col-md-6 icon-box">
+                <div class="icon">
+                  <img src="img/icons/cold.png">
+                </div>
+                  <h4 class="title">Fever</h4>
+                  <p class="description">A fever is a body temperature that is higher than normal. A normal temperature can vary from person to person,but it is usually around 98.6F. </p>
+              </div>
+
+              <div class="col-lg-4 col-md-6 icon-box">
+                <div class="icon">
+              <img src="img/icons/education.png">
+            </div>
+              <h4 class="title">Tiredness</h4>
+              <p class="description" >Feeling overtired, with low energy and a strong desire to sleep that interferes with normal daily activities.</p>
+            </div>
+
+            <div class="col-lg-4 col-md-6 icon-box">
+                <div class="icon">
+              <img src="img/icons/avatar.png">
+            </div>
+              <h4 class="title">Dry Cough</h4>
+              <p class="description" >A sudden, forceful hacking sound to release air and clear an irritation in the throat or airway.</p>
+            </div>
+
+            <div class="col-lg-4 col-md-6 icon-box">
+                <div class="icon">
+              <img src="img/icons//healthcare-and-medical (2).png">
+            </div>
+              <h4 class="title">Aches and Pains</h4>
+              <p class="description" >Muscle pain is most often related to tension, overuse, or muscle injury from exercise or hard physical work.</p>
+            </div>
+          
+            <div class="col-lg-4 col-md-6 icon-box">
+                <div class="icon">
+              <img src="img/icons/discharge.png">
+            </div>
+              <h4 class="title">Nasal Congestion</h4>
+              <p class="description" >Nasal passages swollen with excess fluid and mucus, may be triggered by infection, tobacco smoke or perfume.</p>
+            </div>
+
+            <div class="col-lg-4 col-md-6 icon-box">
+                <div class="icon">
+              <img src="img/icons/discharge (5).png">
+            </div>
+              <h4 class="title">Runny Nose</h4>
+              <p class="description" >Excess drainage, ranging from a clear fluid to thick mucus, from the nose and nasal passages.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      </div>
+    </section><!-- End Features Section -->
+
+
+    <!-- ======= Portfolio Section ======= -->
+    <section id="portfolio" class="portfolio">
+      <div class="container" data-aos="fade-up">
+        <div class="section-header">
+          <h3 class="section-title text-center" >Our Services</h3>
+        </div>
+
+        <div class="row" data-aos="fade-up" data-aos-delay="100">
+          <div class="col-lg-12 d-flex justify-content-center">
+            <ul id="portfolio-flters">
+              <li data-filter="*" class="filter-active">All</li>
+              <li data-filter=".filter-app">Health Care</li>
+              <li data-filter=".filter-card">Disaster Management</li>
+              <li data-filter=".filter-web">Education & Skillng</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <img src="img/lakshadweep/center.jpg" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <h4>Public Health Care Center</h4>
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <img src="img/home/medical.png" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <h4>Medical</h4>
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <img src="img/home/blood.png" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <h4>Blood Bank</h4>
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <img src="img/home/donate.png" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <h4>Donations</h4>
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+             <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <img src="img/home/volunteer.jpg" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <h4>Volunteers</h4>
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <img src="img/chattisgarh/logo.png" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <h4>Government Services</h4>
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+           <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <img src="img/home/mask.jpg" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <h4>Movement Pass</h4>
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+          
+         <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+            <img src="img/home/hr.png" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <h4>HR Recruitment</h4>
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+            <img src="img/home/courses.jpg" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <h4>Online Vocational Training Courses</h4>
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <img src="img/home/msme.jpg" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <h4>MSME Products</h4>
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <img src="img/home/courses.png" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <h4>Essential Workers</h4>
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-card">
+            <img src="img/home/donation.jpg" class="img-fluid" alt="" style="height: 220px; width: 420px;">
+            <div class="portfolio-info">
+              <h4>Food Supply</h4>
+              <a href="#" class="details-link" title="More Details"></a>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Portfolio Section -->
+
+    <!-- ======= Team Section ======= -->
+    <section id="team" class="team">
+      <div class="container">
+
+        <div class="section-title" data-aos="fade-in" data-aos-delay="100">
+          <h2>To Flatten The Curve & Break the Chain</h2>
+          <p>STAY HOME, STAY SAFE</p>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-3">
+            <div class="member" data-aos="fade-up">
+              <div class="member-info">
+                <h4> WASH HANDS OFTEN</h4>
+                
+              </div>
+            </div>
+            <div class="member" data-aos="fade-up" style="height: 100px;">
+              <div class="member-info">
+                <h4> COVER YOUR COUGH</h4>
+              </div>
+            </div>
+          </div> 
+          <div class="col-lg-3"> 
+              <div class="member" data-aos="fade-up"> 
+              <div class="member-info">
+                <h4>KEEP A SAFE DISTANCE</h4>
+               
+              </div>
+            </div>
+             <div class="member" data-aos="fade-up" >
+              <div class="member-info">
+                <h4>SICK? CALL THE HELPLINE</h4>
+                
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-6">
+            <img src="img/home/banner.png" class="img-fluid">
+          </div>  
+
+        </div>
+
+      </div>
+    </section><!-- End Team Section -->
+
+
+  </main><!-- End #main -->
+
+
+<script type="text/javascript">
+  
+$.ajax({
+    url:"https://api.covid19india.org/v2/state_district_wise.json",
+    type:"GET",
+    dataType:"JSON",
+    success : function(covid_data) {
+
+     //console.log(covid_data);
+     var newArray = covid_data.filter(function (el) {
+
+      return el.state === 'Chhattisgarh';
+
+    });
+
+    //console.log(newArray);
+
+    Array.prototype.sum = function (prop) {
+    var total = 0
+    for ( var i = 0, _len = this.length; i < _len; i++ ) {
+        total += this[i][prop]
+    }
+    return total
+    }
+
+    var StateConfirmed = newArray[0].districtData.sum("confirmed");
+    document.getElementById("StateConfirmed").innerHTML = StateConfirmed;
+
+    var StateActive = newArray[0].districtData.sum("active");
+    document.getElementById("StateActive").innerHTML = StateActive;
+
+    var StateDeath = newArray[0].districtData.sum("deceased");
+    document.getElementById("StateDeath").innerHTML = StateDeath;
+
+    var StateRecovered = newArray[0].districtData.sum("recovered");
+    document.getElementById("StateRecovered").innerHTML = StateRecovered;
+
+    // var MainStateConfirmed = newArray[0].districtData.sum("confirmed");
+    // document.getElementById("MainStateConfirmed").innerHTML = "Covid19 Cases - " + (MainStateConfirmed) ;
+}
+
+});
+
+
+</script>
+
+<?php include('includes/footer.php');?>
+<?php include('includes/scripts.php');?>
